a627704ffc245816923d8812de097713fe99c21d
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2021-07-10 14:09:18 +0500

    Created models. Enabled admin model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class BusinessOwner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    business_owner = models.ForeignKey(BusinessOwner, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Agent: {self.user.username}"
