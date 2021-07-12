12db110a91f883349d908dc5b1e8b86334bef7c2
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2021-07-12 17:39:25 +0500

    Added ClientDetailApiView(APIView) with crud

--- clients/urls.py
+++ clients/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from clients.views import ClientListApiView
+from clients.views import ClientListApiView, ClientDetailApiView
 
 
 urlpatterns = [
     path('', ClientListApiView.as_view(), name='clients'),
+    path('<int:id>/', ClientDetailApiView.as_view(), name='detail')
 ]
--- clients/views.py
+++ clients/views.py
@@ -22,3 +22,60 @@ class ClientListApiView(APIView):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class ClientDetailApiView(APIView):
+
+    def get(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(instance=client)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+
+    def put(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(instance=client, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def patch(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientSerializer(instance=client, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, id):
+        try:
+            client = Client.objects.get(pk=id)
+        except Client.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        client.delete()
+
+        return Response(data={'message':'deleted'}, status=status.HTTP_204_NO_CONTENT)
